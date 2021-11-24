61d7075ea28cd5adae7e67d26cf45b78adce9363
Chris <chrisclayhendrik@gmail.com>
2021-11-24 12:33:19 -0600

    Unfinished submission. Ran out of time.

--- tests/scooter.test.js
+++ tests/scooter.test.js
@@ -1,8 +1,25 @@
 const Scooter = require('../src/scooter')
 
 describe('Scooter class', () => {
+    const testScooter = new Scooter(1, 100)
+    const testScooter2 = new Scooter(2, 50)
+
     test('has an id', () => {
         const scooter = new Scooter(1, 100)
         expect(scooter.id).toBe(1)
     })
+
+    test('has a charge', () => {
+        expect(testScooter.charge).toBe(100)
+    })
+
+    test('rent scooter does not fail', () => {
+        expect(testScooter.rent()).toEqual(true)
+    })
+
+    test('return scooter does not fail', () => {
+        expect(testScooter.return()).toEqual(true)
+    })
+
+
 })
