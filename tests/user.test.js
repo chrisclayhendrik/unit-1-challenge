cad168c3e6b100bbfd40c32540bbd53989ef6070
Chris <chrisclayhendrik@gmail.com>
2021-11-24 10:09:34 -0600

    Completed user class

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,14 @@
 const User = require('../src/user')
 
 describe('User class', () => {
-    const testUser = new User('Chris Hendrik', 'chrisclayhendrik@gmail.com', 'Password1!', 1990, 'Dallas')
+    const testUser = new User('Chris Hendrik', 'chrisclayhendrik@gmail.com', 'Password1!', 1990, 'Dallas');
+    const testUser2 = new User('Chris Hendrik', 'chrisclayhendrik@gmail.com', 'Password1!', 2004, 'Dallas');
     
 
     test('has a constructor', () => {
         expect(testUser.constructor.name).toBe('User')
     })
+
     test('has a name', () => {
         expect(testUser.name).toBe(testUser.name)
     })
@@ -29,10 +31,23 @@ describe('User class', () => {
 
     test('registers users', () => {
         testUser.register(testUser);
-        expect(testUser.users).toContainEqual(testUser)
+        expect(User.users).toContainEqual(testUser)
     })
 
     test('has a login function', () => {
-        expect(testUser.login).toBe(testUser.login)
+        expect(testUser.login()).toBe(testUser.login())
     })
+
+    test('has a logout function', () => {
+        expect(testUser.logout()).toBe(testUser.logout())
+    })
+
+    test('user validation succeeds', () => {
+        expect(testUser.userValidation('chrisclayhendrik@gmail.com', 'Password1!')).toBe(true)
+    })
+
+    test('user validation fails', () => {
+        expect(testUser2.userValidation('chrisclayhendrik@gmail.com', 'Password2!')).toBe(false)
+    })
+    
 })
