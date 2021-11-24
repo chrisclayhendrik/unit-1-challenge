48eb94d51159ae169a611a45962a430d7f25ae76
Chris <chrisclayhendrik@gmail.com>
2021-11-24 08:59:07 -0600

    updated testing

--- tests/payment.test.js
+++ tests/payment.test.js
@@ -1,4 +1,8 @@
 const Payment = require('../src/payment');
 
 describe('Payment class', () => {
+
+    test('has a method', () => {
+        expect(this.method).toBe(this.method);
+    })
 })
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,38 @@
 const User = require('../src/user')
 
 describe('User class', () => {
-
+    const testUser = new User('Chris Hendrik', 'chrisclayhendrik@gmail.com', 'Password1!', 1990, 'Dallas')
     
-})
 
-    test('has a name', () => {
-        const testUser = new User('Chris Hendrik', 'chrisclayhendrik@gmail.com', '05-17-1990', 'Dallas')
-        expect(testUser.name).toBe('Chris Hendrik')
+    test('has a constructor', () => {
+        expect(testUser.constructor.name).toBe('User')
     })
+    test('has a name', () => {
+        expect(testUser.name).toBe(testUser.name)
+    })
+
+    test('has an email', () => {   
+        expect(testUser.email).toBe(testUser.email)
+    })
+
+    test('has a password', () => {  
+        expect(testUser.password).toBe(testUser.password)
+    })
+
+    test('has a birthyear', () => {  
+        expect(testUser.birthYear).toBe(testUser.birthYear)
+    })
+
+    test('has a city', () => { 
+        expect(testUser.city).toBe(testUser.city)
+    })
+
+    test('registers users', () => {
+        testUser.register(testUser);
+        expect(testUser.users).toContainEqual(testUser)
+    })
+
+    test('has a login function', () => {
+        expect(testUser.login).toBe(testUser.login)
+    })
+})
